241a442addcbc7b6cc7bb7dc0627386e34c1c4e2
techarif1 <96561084+techarif1@users.noreply.github.com>
2022-01-18 14:18:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,31 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/gcp"
-      version = "~> 4.6.0"
+      source = "hashicorp/google"
+      version = "3.5.0"
     }
   }
+}
 provider "google" {
   credentials = "${file("serivice-account.json)}"
   project     = "skilful-sensor-335405"
   region      = "us-central1"
+  zone    = "us-central1-c"
 }
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+resource "google_compute_instance" "default" {
+  name         = "test"
   machine_type = "f1-micro"
-
+  zone         = "us-central1-a"
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
     }
   }
-
   network_interface {
-    # A default network is created for all GCP projects
     network = "default"
+
     access_config {
+      // Ephemeral public IP
     }
   }
-}
 }
