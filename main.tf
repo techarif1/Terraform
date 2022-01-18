49bd68199fb5694f8cf9dadd3d478647946f9f74
techarif1 <96561084+techarif1@users.noreply.github.com>
2022-01-18 14:06:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,22 +4,21 @@ provider "google" {
   project     = "skilful-sensor-335405"
   region      = "us-central1"
 }
-
-resource "google_compute_instance" "default" {
-  name         = "arif18jan"
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
   machine_type = "f1-micro"
-  zone         = "us-central1-a"
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
     }
   }
-   network_interface {
-    network = "default"
 
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
     access_config {
-      // Ephemeral public IP
     }
   }
- } 
+}
 }
